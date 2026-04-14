ad24c9b52d183428d0c392cee1b5f91033f528da
Idrocx1710 <rogervz.1710@gmail.com>
2026-04-13 21:04:33 -0500

    Creación del Proyecto SpringBoot

---
+++ src/main/java/com/herramientas/optica/OpticaApplication.java
@@ -0,0 +1,13 @@
+package com.herramientas.optica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpticaApplication.class, args);
+	}
+
+}
